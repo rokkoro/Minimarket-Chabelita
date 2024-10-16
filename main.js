ba2080913723378f3388ae47d2fc3391b336ca71
rokkoro <mpolosi@unitru.edu.pe>
2024-10-16 11:50:12 -0500

    Cambios listo para publicar

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 let menu = document.querySelectorAll('#menu-icon');
+let navbar = document.querySelectorAll('.navbar');
 
-//
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+window.onscroll  = () =>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active')
+}
